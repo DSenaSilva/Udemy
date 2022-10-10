eb69f3f279d2aa0455d24a3cffe389acc50a7d33
Daiane Sena <78742341+DSenaSilva@users.noreply.github.com>
2022-10-10 16:13:37 -0300

    init

---
+++ Web/Fundamentos/inicio/Comentario.js
@@ -0,0 +1,16 @@
+
+// console.log é usado para exibir algo na tela do computador
+console.log('Passo #01');
+console.log('Passo #02');// mais um exemplo de comentario
+//console.log('Passo #03');
+
+
+/*
+Exemplo
+de 
+multiplas linhas
+de 
+Comentários
+
+ */
+console.log('Passo #03');
---
+++ Web/Fundamentos/inicio/objeto.js
@@ -0,0 +1,9 @@
+/*var objeto = {
+    nome: "Caneta",
+    quantidade: 10,
+    preco: 6.4,
+    imposto: 1.5,
+    precoFinal: this.preco + this.imposto
+}
+
+console.log(objeto);*/
